ba87016b3e593cc6c40837f2b9f5060a8ea127f8
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-15 07:37:32 -0700

    Func

--- _putchar.c
+++ _putchar.c
@@ -1,36 +1,32 @@
 #include "main.h"
-
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * @ch: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char ch)
 {
-	return (buffer(c));
+	return (buffer(ch));
 }
-
 /**
  * buffer - Save the character in a buffer
- * @c: Character
+ * @ch: Character
  *
  * Return: 1
  **/
-int buffer(char c)
+int buffer(char ch)
 {
 	static char buffering[1024];
 	static int i;
 
-	if (c == -1 || i == 1024)
+	if (ch == -1 || i == 1024)
 	{
 		write(1, buffering, i);
 		i = 0;
 	}
-
-	if (c != -1)
-		buffering[i++] = c;
+	if (ch != -1)
+		buffering[i++] = ch;
 
 	return (1);
 }
