d04d09018fdc8e7017ba09786e4b3bde3d61aaea
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-15 04:54:20 -0700

    all

--- _putchar.c
+++ _putchar.c
@@ -1,29 +1,13 @@
 #include <unistd.h>
-/**
- * _putchar - prints char
- * @ch: printed char
- * Return: output
- */
-int _putchar(char ch)
-{
-	static int contador;
-	static char buffer[1024];
 
-	if (ch == 1)
-	{
-		contador = 0;
-		return (0);
-	}
-	if (ch == -2 || contador == 1024)
-	{
-		write(1, buffer, contador);
-		contador = 0;
-	}
-	if (ch != -1 && ch != 2)
-	{
-		buffer[contador] = ch;
-		contador++;
-		return (1);
-	}
-	return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));	
 }
