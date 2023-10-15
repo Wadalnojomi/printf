a46762ec082e0de61988493ac5cec8687bc40a01
alaakamel221 <alaamohamedkamel22@gmail.com>
2023-10-15 15:16:29 -0700

    ggg

--- _printf.c
+++ _printf.c
@@ -1,49 +1,53 @@
 #include "main.h"
 /**
- * _printf - printf like function
- * @formate:  is a character string
- * Return: the number of characters printed
- */
-int _printf(const char *formate, ...)
+* _printf - printf like function
+* @format:  is a character string
+* Return: the number of characters printed
+*/
+int _printf(const char *format, ...)
 {
-	int counter = 0, c;
-	char *s;
+	int counter = 0;
 	va_list args;
 
-	if (formate == NULL)
+	if (format == NULL)
 		return (-1);
-	va_start(args, formate);
-	while (*formate != '\0')
+	va_start(args, format);
+	while (*format != '\0')
 	{
-		if (*formate != '%')
+		if (*format != '%')
 		{
-			_putchar(*formate);
+			write(1, format, 1);
 			counter++;
 		}
 		else
-			formate++;
-		if (*formate == 'c')
 		{
-			c = va_arg(args, int);
-				_putchar(c);
+			format++;
+			if (*format == '\0')
+			break;
+		if (*format == 'c')
+		{
+			char c = va_arg(args, int);
+				write(1, &c, 1);
 			counter++;
 		}
-		else if (*formate == 's')
+		else if (*format == 's')
 		{
-			*s = va_arg(args, int);
-			while (*s != '\0')
+		char *str = va_arg(args, char*);
+		int str_len = 0;
+			while (str[str_len] != '\0')
 			{
-				_putchar(*s);
-				s++;
-				counter++;
+				str_len++;
+				write(1, str, str_len);
+				counter += str_len;
 			}
 		}
-		else if (*formate == '%')
+		else if (*format == '%')
 		{
-			_putchar('%');
+				write(1, format, 1);
 			counter++;
 		}
-		formate++;
+		}
+		format++;
 	}
 	va_end(args);
 	return (counter);
--- main.h
+++ main.h
@@ -1,16 +1,22 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <limits.h>
+#define OUTPUT_BUF_SIZE
+#define NULL_STRING "(null)"
 /**
  * struct structprint - struct specifier
  * @q: method to translate data
  * @u: print function
  * Return: an integer
- */
+*/
+/* print_functions.c_module */
+
+
 typedef struct structprint
 {
 	char *q;
@@ -20,7 +26,7 @@ int _putchar(char ch);
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
+int binary(int num);
+int octal(int num);
 
-
-#endif
-
+#endif /* MAIN_H */
