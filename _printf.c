ecdb6ddd835ad91819ab072e46d5588cd412a79e
Wadalnojomi <dr.mhmalnojomi@gmail.com>
2023-10-16 02:51:16 -0700

    me

--- _printf.c
+++ _printf.c
@@ -1,7 +1,9 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
-
+#include "main.h"
+/**
+ * _printf - recreates the printf funct
+ * @format: format specifier
+ * Return: number of chars printed
+ */
 int _printf(const char *format, ...)
 {
 	int counter = 0, c;
@@ -9,33 +11,41 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	for (p = format; *p; ++p) {
-		if (*p != '%') {
-            putchar(*p);
-            ++counter;
-        } else {
-            switch (*++p) {
-                case 'c': {
-                    c = va_arg(args, int);
-                    putchar(c);
-                    ++counter;
-                    break;
-                }
-                case 's': {
-                    s = va_arg(args, const char *);
-                    fputs(s, stdout);
-                    counter += strlen(s);
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    ++counter;
-                    break;
-                }
-            }
+	for (p = format; *p; ++p)
+	{
+		if (*p != '%')
+		{
+			putchar(*p);
+			++counter;
         }
-    }
-    va_end(args);
-    return counter;
+		else
+		{
+			switch (*++p)
+			{
+				case 'c':
+					{
+						c = va_arg(args, int);
+						putchar(c);
+						++counter;
+						break;
+					}
+				case 's':
+					{
+						s = va_arg(args, const char *);
+						fputs(s, stdout);
+						counter += strlen(s);
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						++counter;
+						break;
+					}
+			}
+		}
+	}
+	va_end(args);
+	return (counter);
 }
 
